43362cd129748f242ed91a7e1f2fc7e907aa1dd1
William Kevin Manire <williamkmanire@gmail.com>
2015-05-10 22:40:29 -0700

    Added basic setup.py and created pygameanimation package to facilitate installation.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name="pygame-animation",
+      version="0.0.1",
+      description="PyGame Animation and Tweening Library",
+      author="bitcraft",
+      author_email="",
+      packages=['pygameanimation'],
+      license="GPLv3",
+      url="https://github.com/bitcraft/animation",
+      download_url="",
+      long_description="",
+      package_data={},
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Development Status :: 5 - Production/Stable",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Natural Language :: English",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.4",
+          "Topic :: Games/Entertainment",
+          "Topic :: Software Development :: Libraries",
+      ],
+)
